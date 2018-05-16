6511ef1ec881860c7134a02bdf92d5cb88bc9846
CristianSR4 <cristian.lerida.sanchez@gmail.com>
2018-05-16 09:16:16 +0200

    Proyecto1

---
+++ Proyecto1/src/Principal.java
@@ -0,0 +1,21 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Tipos de variables más conocidas
+		int numero = 0;
+		int numero2 = 10;
+		double decimal = 9.5;
+		char caracter = '@';
+		String palabra = "achus";
+		boolean soyHumano = true;
+		String alumno = "guzman";
+		
+		for(int i = alumno.length()-1;i>=0;i--) {
+			char letra = alumno.charAt(i);
+			System.out.println(letra);
+	}
+	}
+}
